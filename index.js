3938fd24efbf8140eb51fdc9a6cceeb9a4041b3f
pomidorka2010 <lamc560ki@hotmail.com>
2025-09-26 13:53:44 +0200

    Refactored error handling middleware to provide a more descriptive error message.

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.use('/api/users', userRoutes);
 // Error handling middleware
 app.use((err, req, res, next) => {
   console.error(err.stack);
-  res.status(500).send('Something broke!');
+  res.status(500).send(`Internal Server Error: ${err.message}`);
 });
 
 const PORT = process.env.PORT || 3000;
