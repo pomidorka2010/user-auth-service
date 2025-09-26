7e81e432cbd4cca98fcb2dca8582ef8f4a2da42a
pomidorka2010 <lamc560ki@hotmail.com>
2025-09-26 13:51:29 +0200

    feat: initial commit for index.js

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const userRoutes = require('./routes/userRoutes');
+
+dotenv.config();
+
+const app = express();
+
+// Middleware
+app.use(express.json());
+
+// Database connection
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.log(err));
+
+// Routes
+app.use('/api/users', userRoutes);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
